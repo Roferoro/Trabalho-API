1e2c98f2c2c4f00aa96a3ad5734885778bc99f15
GustavoPisanello <99992149+GustavoPisanello@users.noreply.github.com>
2023-06-22 09:26:04 -0300

    git commit true app

--- app/src/main/java/com/example/searchcountries2/adapter/CountryAdapter.java
+++ app/src/main/java/com/example/searchcountries2/adapter/CountryAdapter.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-// import com.bumptech.glide.Glide;
+import com.bumptech.glide.Glide;
 import com.example.searchcountries2.R;
 import com.example.searchcountries2.model.CountryInfo;
 import com.example.searchcountries2.network.DetailActivity;
@@ -40,7 +40,7 @@ public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.Holder> 
         String flag = countryInfoList.get(position).getFlag();
         String name = countryInfoList.get(position).getName();
         holder.countryName.setText(name);
-    //    Glide.with(context).load(flag).into(holder.countryFlag);
+        Glide.with(context).load(flag).into(holder.countryFlag);
     }
 
     @Override
